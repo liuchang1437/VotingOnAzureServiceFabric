bd593c9b97164d00e16d478e6977b9923eb6ed59
Chang Liu <t-chali@microsoft.com>
2018-07-18 14:11:39 +0800

    Build application using Service Fabric

--- VotingWeb/VotingWeb.cs
+++ VotingWeb/VotingWeb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net.Http;
+using System;
 using System.Collections.Generic;
 using System.Fabric;
 using System.IO;
@@ -30,23 +31,34 @@ namespace VotingWeb
         {
             return new ServiceInstanceListener[]
             {
-                new ServiceInstanceListener(serviceContext =>
-                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
-                    {
-                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+             new ServiceInstanceListener(
+                serviceContext =>
+                    new KestrelCommunicationListener(
+                        serviceContext,
+                        "ServiceEndpoint",
+                        (url, listener) =>
+                        {
+                            ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
 
-                        return new WebHostBuilder()
-                                    .UseKestrel()
-                                    .ConfigureServices(
-                                        services => services
-                                            .AddSingleton<StatelessServiceContext>(serviceContext))
-                                    .UseContentRoot(Directory.GetCurrentDirectory())
-                                    .UseStartup<Startup>()
-                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
-                                    .UseUrls(url)
-                                    .Build();
-                    }))
+                            return new WebHostBuilder()
+                                .UseKestrel()
+                                .ConfigureServices(
+                                    services => services
+                                        .AddSingleton<HttpClient>(new HttpClient())
+                                        .AddSingleton<FabricClient>(new FabricClient())
+                                        .AddSingleton<StatelessServiceContext>(serviceContext))
+                                .UseContentRoot(Directory.GetCurrentDirectory())
+                                .UseStartup<Startup>()
+                                .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                .UseUrls(url)
+                                .Build();
+                        }))
             };
+        }
+
+        internal static Uri GetVotingDataServiceName(ServiceContext context)
+        {
+            return new Uri($"{context.CodePackageActivationContext.ApplicationName}/VotingData");
         }
     }
 }
